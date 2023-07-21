cdf124e343ad93c0e147322c2f33ff0f084112be
abdelhay bouramdane <109427562+abouramd@users.noreply.github.com>
2023-07-21 12:18:25 +0000

    @abouramd

--- _printf.c
+++ _printf.c
@@ -8,5 +8,46 @@
 
 int _printf(const char *format, ...)
 {
-    
+	t_data data;
+	t_flag flag[] = {
+		{'c', NULL},
+		{'i', NULL},
+		{'d', NULL},
+		{'s', NULL},
+		{0, NULL}
+	};
+	int i;
+	bool b;
+
+	va_start(data.p, format);
+	data.format = format;
+	data.index = 0;
+	data.len = 0;
+	while (data.format[data.index])
+	{
+		i = 0;
+		b = true;
+		if (data.format[data.index] == '%')
+		{
+			while (flag[i].c)
+			{
+				if (flag[i].c == data.format[data.index + 1])
+				{
+					if (flag[i].ptr)
+						flag[i].ptr(data);
+					else
+						write(1, "(nil)", 5);
+					data.index++;
+					b = false;
+					break;
+				}
+				i++;
+			}
+		}
+		if (b)
+			data.len += write(1, &data.format[data.index], 1);
+		data.index++;
+	}
+	va_end(data.p);
+	return (data.len);
 }
--- main.h
+++ main.h
@@ -1,7 +1,39 @@
-
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+/**
+ * strct data - struct
+ * @p: va_list
+ * @index: int
+ * @format: format ptr
+ * @len: int
+ */
+
+typedef struct data
+{
+    va_list p;
+    const char *format;
+    int index;
+    int len;
+} t_data;
+
+/**
+ * strct data - struct
+ * @c: the flag
+ * @ptr: pointer to function
+ */
+ 
+typedef struct flag
+{
+    char c;
+    void (*ptr)(t_data);
+} t_flag;
+
+
 int _printf(const char *format, ...);
 
-#endifsemanticTokensProvider
+#endif
