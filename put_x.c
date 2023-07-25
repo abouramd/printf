ddae8fa5bc93d7c78a3c94c96f4246f8b6a72c32
Youssef yahya <92722565+JosepharDev@users.noreply.github.com>
2023-07-25 09:26:49 +0100

    Update put_x.c

--- put_x.c
+++ put_x.c
@@ -3,5 +3,7 @@
 void put_x(t_data *data)
 {
 	unsigned int n = va_arg(data->p, unsigned int);
+	if (data->hash && n)
+		data->len += write(1, "0X", 2);
 	put_base(data, n, "0123456789abcdef", 16);
 }
