b318e35060dd3f7612d773471b1b4c6c5b8eae34
abdelhay bouramdane <109427562+abouramd@users.noreply.github.com>
2023-07-25 10:40:31 +0000

    low level

--- put_p.c
+++ put_p.c
@@ -10,6 +10,11 @@ void put_p(t_data *data)
 {
 	unsigned long n = va_arg(data->p, unsigned long);
 
-	data->len += write(1, "0x", 2);
-	put_ubase(data, n, "0123456789abcdef", 16);
+	if (!n)
+	data->len += write(1, "(nil)", 5);
+	else
+	{
+		data->len += write(1, "0x", 2);
+		put_ubase(data, n, "0123456789abcdef", 16);
+	}
 }
